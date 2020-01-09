4a4c98e59ad0b15db672d060bb13ef20f1285406
Tai Thach <tthach@hq.bill.com>
2020-01-09 23:58:31 +0000

    Update VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,19 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1b"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
